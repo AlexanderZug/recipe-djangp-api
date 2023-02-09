40929ba3324fdeb20e182bc11ce2bd0fae4b3686
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-02-09 15:51:50 +0500

    fix: flake8

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -35,7 +35,6 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-
     def test_user_with_existing_email(self):
         """Test creating a user that already exists fails."""
         payload = {
@@ -58,5 +57,7 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
         self.assertFalse(user_exists)
