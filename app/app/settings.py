94885b0ac1e05dc4037067d1b27ed693291c5a51
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-02-19 18:22:18 +0100

    db fail

--- app/app/settings.py
+++ app/app/settings.py
@@ -140,3 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+DISABLE_SERVER_SIDE_CURSORS = True
