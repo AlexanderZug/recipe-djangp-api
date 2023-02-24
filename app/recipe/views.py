9b572510ef40a86b394986561c91f2614b7deaeb
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-02-24 14:35:26 +0100

    feat: add filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -101,6 +101,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='assigned_only',
+                type=int,
+                enum=[0, 1],
+                description='1 to return only assigned tags/ingredients',
+            )
+        ]
+    )
+)
 class BaseRecipeAtrrViewSet(mixins.DestroyModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.ListModelMixin,
@@ -111,7 +123,15 @@ class BaseRecipeAtrrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Retrieve the ingredients or tags for the authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAtrrViewSet):
